80ce3cbeaa9d33bfe74a91390cb4e589d981fead
imran sk <imran.bu96@gmail.com>
2017-10-21 11:51:08 +0600

    initial commit

--- app/src/main/java/com/example/imransk/locationusinggooglemapapi/MapsActivity.java
+++ app/src/main/java/com/example/imransk/locationusinggooglemapapi/MapsActivity.java
@@ -136,8 +136,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
 
     }
-
-
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
